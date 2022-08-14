a7c3f72934e3570a1deb7c30480639025b6fdd44
Misha Panivnyk <panivnykm@gmail.com>
2022-08-14 13:46:21 +0300

    add swiper

--- src/slider.js
+++ src/slider.js
@@ -5,10 +5,17 @@ const swiper1 = new Swiper('.swiper1', {
   speed: 300,
 });
 const swiper2 = new Swiper('.swiper2', {
+  slidesPerView: 6,
   spaceBetween: 30,
+  slidesPerGroup: 3,
+  loopFillGroupWithBlank: true,
   pagination: {
-    el: '.swiper-pagination2',
+    el: '.swiper-pagination',
     clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
 // const swiper3 = new Swiper('.swiper3', {
