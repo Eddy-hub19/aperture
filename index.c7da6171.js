e9996509a6fbc99e7c2f11b5d421604b336e0d20
Eddy-hub19 <75374150+Eddy-hub19@users.noreply.github.com>
2022-08-14 18:57:54 +0000

    Deploying to gh-pages from @ Eddy-hub19/aperture@2d2dc9af6217182ea3bf8bcaf4aae5da17b81e3c 🚀

--- index.c7da6171.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper1",{spaceBetween:0,autoplay:1,loop:!0,speed:300}),new Swiper(".swiper2",{spaceBetween:0,speed:300,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.c7da6171.js.map
