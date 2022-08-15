00f1a460a4efc4e315112af73d1648ca1664c415
Eddy-hub19 <75374150+Eddy-hub19@users.noreply.github.com>
2022-08-15 15:02:43 +0000

    Deploying to gh-pages from @ Eddy-hub19/aperture@97ba513208d8fb6fdb4b4e6dd9e5aa937002b5a7 🚀

--- index.01e1ea08.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper1",{autoplay:!0,spaceBetween:0,loop:!0,speed:300}),new Swiper(".swiper2",{spaceBetween:0,autoplay:!0,speed:300,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.01e1ea08.js.map
---
+++ index.19f32fd7.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper1",{autoplay:!0,loop:!0,speed:800,effect:"cube",cubeEffect:{slideShadows:!1},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}}),new Swiper(".swiper2",{autoplay:!0,loop:!0,speed:300,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}}),new Swiper(".swiper3",{autoplay:!0,loop:!0,speed:300});
+//# sourceMappingURL=index.19f32fd7.js.map
